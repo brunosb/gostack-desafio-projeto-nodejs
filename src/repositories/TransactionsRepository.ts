bb271c90ab21139144247e993bd585b369e1ec19
Bruno Barbosa <barbosa.developer@gmail.com>
2020-04-13 22:49:19 -0300

    Configuracao expressao ternaria e otimizacao do reduce no getBalance().

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -24,17 +24,18 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    const income = this.transactions
-      .filter(transaction => transaction.type === 'income')
-      .reduce((acc, transaction) => acc + transaction.value, 0);
+    const balance = this.transactions.reduce(
+      (acc, transaction) => {
+        const { type } = transaction;
+        type === 'income'
+          ? (acc.income += transaction.value)
+          : (acc.outcome += transaction.value);
+        return acc;
+      },
+      { income: 0, outcome: 0 },
+    );
 
-    const outcome = this.transactions
-      .filter(transaction => transaction.type === 'outcome')
-      .reduce((acc, transaction) => acc + transaction.value, 0);
-
-    const total = income - outcome;
-
-    return { income, outcome, total };
+    return { ...balance, total: balance.income - balance.outcome };
   }
 
   public create({ title, value, type }: CreateTransactionDTO): Transaction {
